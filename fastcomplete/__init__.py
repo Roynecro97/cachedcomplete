da31eb369fd017311d089ef95b5fa7523d81c2a7
Swey9 <18035997+Swey9@users.noreply.github.com>
2020-03-17 15:40:20 +0200

    Added the logic for __init__.py

--- fastcomplete/__init__.py
+++ fastcomplete/__init__.py
@@ -1,11 +1,19 @@
 import argcomplete
 import os
 
+from .parser_cache import save_cache, load_cache
 
-class FastcompleteCompletionFinder(argcomplete.CompletionFinder):
+
+# This class derives from CompletionFinder and allows caching its data to prevent multiple runs of the same script
+class CachedCompletionFinder(argcomplete.CompletionFinder):
     def __call__(self, *args, **kwargs):
+        parser_cache.save_cache(args, kwargs)
         return super().__call__(*args, **kwargs)
 
 
-autocomplete = FastcompleteCompletionFinder()
-autocomplete.__doc__ = """ Use this to access fastcomplete. See :meth:`fastcomplete.FastcompleteCompletionFinder.__call__()`. """
+autocomplete = CachedCompletionFinder()
+autocomplete.__doc__ = """ Use this to access fastcomplete. See :meth:`fastcomplete.CachedCompletionFinder.__call__()`. """
+
+loaded_cache = parser_cache.load_cache()
+if loaded_cache is not None:
+    autocomplete(*loaded_cache)
