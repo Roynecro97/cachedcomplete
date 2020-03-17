fb7bfd09950b78ee8574ee8c4e56d6ad8349852c
Swey9 <18035997+Swey9@users.noreply.github.com>
2020-03-17 16:10:13 +0200

    Turns CachedCompletionFinder to a decorator and adds its usage to the README

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # fastcomplete
 Fast semi-static autocomplete for python
+
+## Usage of custom subclasses of CompletionFinder
+In order to use custom CompletionFinders with fastcomplete, 
+a usage of the decorator cached_completion_finder is required.
+### For example:
+#### Using argcomplete:
+```python
+import argcomplete
+
+class CustomCompletionFinder(argcomplete.CompletionFinder):
+    ...
+
+completion_finder = CustomCompletionFinder()
+# Instead of argcomplete.autocomplete()
+completion_finder()
+```
+#### Using fastcomplete:
+```python
+import argcomplete
+import fastcomplete
+
+@fastcomplete.cached_completion_finder
+class CustomCompletionFinder(argcomplete.CompletionFinder):
+    ...
+
+completion_finder = CustomCompletionFinder()
+# Instead of argcomplete.autocomplete()
+completion_finder()
+```
+##### or
+```python
+import argcomplete
+import fastcomplete
+
+class CustomCompletionFinder(argcomplete.CompletionFinder):
+    ...
+
+completion_finder = fastcomplete.cached_completion_finder(CustomCompletionFinder)()
+# Instead of argcomplete.autocomplete()
+completion_finder()
+```
--- fastcomplete/__init__.py
+++ fastcomplete/__init__.py
@@ -1,18 +1,29 @@
 import argcomplete
 import os
 
+from functools import wraps
+
 from .parser_cache import save_cache, load_cache
 
 
-# This class derives from CompletionFinder and allows caching its data to prevent multiple runs of the same script
-class CachedCompletionFinder(argcomplete.CompletionFinder):
-    def __call__(self, *args, **kwargs):
-        parser_cache.save_cache(args, kwargs)
-        return super().__call__(*args, **kwargs)
+def cached_complation_finder(completion_finder_cls):
+    '''
+    This decorator wraps argcomplete.CompletionFinder and adds the cache save logic to its __call__
+    '''
+    if not issubclass(completion_finder_cls, argcomplete.CompletionFinder):
+        raise TypeError("cached_completion_finder can only be used on classes that derive from CompletionFinder")
+
+    @wraps(completion_finder_cls, updated=())
+    class CachedCompletionFinder(completion_finder_cls):
+        def __call__(self, *args, **kwargs):
+            parser_cache.save_cache(args, kwargs)
+            return super().__call__(*args, **kwargs)
+    
+    return CachedCompletionFinder
 
 
-autocomplete = CachedCompletionFinder()
-autocomplete.__doc__ = """ Use this to access fastcomplete. See :meth:`fastcomplete.CachedCompletionFinder.__call__()`. """
+autocomplete = cached_complation_finder(argcomplete.CompletionFinder)()
+autocomplete.__doc__ = argcomplete.autocomplete.__doc__
 
 loaded_cache = parser_cache.load_cache()
 if loaded_cache is not None:
