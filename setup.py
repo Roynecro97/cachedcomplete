ec638da47ea73a2fbb735f8e0f8bd43a0e03dd25
ohad <ohad.peleg@gmail.com>
2020-05-28 13:08:12 +0300

    Fix setup.py pins on argcomplete
    this is kind of bad practice and can cause weird stuff if you have later isntalled version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-        'argcomplete==1.11.1'
+        'argcomplete>=1.11.1'
     ],
     packages=setuptools.find_packages(exclude=['test']),
     zip_safe=False,
